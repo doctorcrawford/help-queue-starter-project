c1c5abe7db7978c9f94c889e27b05f93d19df45f
Kyle Crawford <kcraw217@gmail.com>
2023-07-19 14:55:36 -0700

    add theme context

---
+++ src/components/ToggleTheme.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function ToggleTheme(props) {
+  const { toggleTheme } = props;
+
+  return (
+    <>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <hr />
+    </>
+  );
+}
+
+ToggleTheme.propTypes = {
+  toggleTheme: PropTypes.func
+}
+
+export default ToggleTheme
